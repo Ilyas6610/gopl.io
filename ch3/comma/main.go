07694cfd9c0deabcf87ada006296bfafc53290a8
Ilyas6610 <iaisakhodzhiev@phystech.edu>
2024-08-03 16:49:58 +0400

    Add ex. 3.10 and 3.11

--- ch3/comma/main.go
+++ ch3/comma/main.go
@@ -6,28 +6,31 @@
 // Comma prints its argument numbers with a comma at each power of 1000.
 //
 // Example:
-// 	$ go build gopl.io/ch3/comma
-//	$ ./comma 1 12 123 1234 1234567890
-// 	1
-// 	12
-// 	123
-// 	1,234
-// 	1,234,567,890
 //
+//	$ go build gopl.io/ch3/comma
+//	$ ./comma 1 12 123 1234 1234567890
+//	1
+//	12
+//	123
+//	1,234
+//	1,234,567,890
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"os"
+	"unicode"
 )
 
 func main() {
 	for i := 1; i < len(os.Args); i++ {
 		fmt.Printf("  %s\n", comma(os.Args[i]))
+		fmt.Printf("  %s\n", nonrecursive_comma(os.Args[i]))
 	}
 }
 
-//!+
+// !+
 // comma inserts commas in a non-negative decimal integer string.
 func comma(s string) string {
 	n := len(s)
@@ -38,3 +41,23 @@ func comma(s string) string {
 }
 
 //!-
+
+func nonrecursive_comma(s string) string {
+	var buf bytes.Buffer
+	n, i := 0, 3-len(s)%3
+	if i == 3 {
+		i = 0
+	}
+	for n < len(s) {
+		if i == 3 {
+			buf.Write([]byte{','})
+			i = 0
+		}
+		if unicode.IsDigit(rune(s[n])) {
+			i++
+		}
+		buf.Write([]byte{s[n]})
+		n++
+	}
+	return buf.String()
+}
